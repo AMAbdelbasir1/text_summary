91f6e59764c6af6cab52eae81926bff444a40213
AMAbdelbasir1 <addtooo140@gmail.com>
2024-05-19 18:36:05 +0300

    first commit

---
+++ enwar.py
@@ -0,0 +1,134 @@
+import spacy
+from flask import Flask, request, jsonify
+from heapq import nlargest
+from camel_tools.tokenizers.word import simple_word_tokenize
+from camel_tools.utils.dediac import dediac_ar
+from langdetect import detect
+import re
+import os
+
+app = Flask(__name__)
+
+# Load the English language model
+nlp_en = spacy.load('en_core_web_sm')
+
+
+def summarize_text_english(text, summary_ratio=0.3):
+    doc = nlp_en(text)
+    word_frequencies = {}
+    for word in doc:
+        word_lower = word.text.lower()
+        if word_lower not in nlp_en.Defaults.stop_words and not word.is_punct:
+            word_frequencies[word_lower] = word_frequencies.get(
+                word_lower, 0) + 1
+
+    if not word_frequencies:
+        return ""
+
+    max_frequency = max(word_frequencies.values())
+
+    for word in word_frequencies:
+        word_frequencies[word] /= max_frequency
+
+    sentence_scores = {}
+    for sentence in doc.sents:
+        for word in sentence:
+            word_lower = word.text.lower()
+            if word_lower in word_frequencies:
+                sentence_scores[sentence] = sentence_scores.get(
+                    sentence, 0) + word_frequencies[word_lower]
+
+    num_sentences = max(1, int(len(list(doc.sents)) * summary_ratio))
+    summary_sentences = nlargest(
+        num_sentences, sentence_scores, key=sentence_scores.get)
+    final_summary = ' '.join([sentence.text for sentence in summary_sentences])
+
+    return final_summary
+
+
+def summarize_text_arabic(text, summary_ratio=0.3):
+    text = dediac_ar(text)  # Remove diacritics
+    words = simple_word_tokenize(text)
+    # print(words)
+    word_frequencies = {}
+    for word in words:
+        if word not in word_frequencies:
+            word_frequencies[word] = 1
+        else:
+            word_frequencies[word] += 1
+
+    if not word_frequencies:
+        return ""
+
+    max_frequency = max(word_frequencies.values())
+
+    for word in word_frequencies:
+        word_frequencies[word] /= max_frequency
+
+    # Split text into sentences using Arabic period
+    sentences = text.split('.')
+    sentence_scores = {}
+    for sentence in sentences:
+        sentence_words = simple_word_tokenize(sentence)
+        for word in sentence_words:
+            if word in word_frequencies:
+                if sentence not in sentence_scores:
+                    sentence_scores[sentence] = word_frequencies[word]
+                else:
+                    sentence_scores[sentence] += word_frequencies[word]
+
+    num_sentences = max(1, int(len(sentences) * summary_ratio))
+    summary_sentences = nlargest(
+        num_sentences, sentence_scores, key=sentence_scores.get)
+    final_summary = ' '.join(summary_sentences)
+
+    return final_summary
+
+
+def summarize_text(text, lang='en', summary_ratio=0.3):
+    if lang == 'en':
+        return summarize_text_english(text, summary_ratio)
+    elif lang == 'ar':
+        return summarize_text_arabic(text, summary_ratio)
+    else:
+        return "Language not supported"
+
+
+def clean_response(text):
+    cleaned_text = re.sub(r'[\r\n"]', '', text)
+    return cleaned_text
+
+
+@app.route('/summarize', methods=['POST'])
+def handle_summarize_request():
+    try:
+        uploaded_file = request.files.get('file')
+
+        if not uploaded_file or uploaded_file.filename == '':
+            return jsonify({'status': 'fail', 'error': 'No file uploaded or selected'}), 400
+
+        text = uploaded_file.read().decode('utf-8')
+
+        language = detect(text)
+
+        if language not in ['en', 'ar']:
+            return jsonify({'status': 'fail', 'error': 'Language not supported'}), 400
+
+        summary = summarize_text(text, lang=language)
+        cleaned_summary = clean_response(summary)
+
+        return jsonify({
+            'status': 'success',
+            'summary': cleaned_summary,
+            'lengthSUMMARY': len(cleaned_summary),
+            'lengthTEXT': len(text),
+            'language': language,
+            # 'original': text
+        }), 200
+    except Exception as e:
+        return jsonify({'status': 'fail', 'error': 'Something went wrong, please try again'}), 500
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
